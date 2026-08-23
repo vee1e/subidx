5d9d7dd34b084012ed71e89795ebdabf25ecf997
lakshit verma <vermalucky2004@gmail.com>
2026-08-24 00:39:08 +0530

    store: gofmt after sort import

--- internal/store/store.go
+++ internal/store/store.go
@@ -261,11 +261,11 @@ const DefaultScanLimit = 100000
 
 type resultHeap []Result
 
-func (h resultHeap) Len() int            { return len(h) }
-func (h resultHeap) Less(i, j int) bool  { return h[i].Seq < h[j].Seq }
-func (h resultHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *resultHeap) Push(x any)         { *h = append(*h, x.(Result)) }
-func (h *resultHeap) Pop() any           { old := *h; n := len(old); x := old[n-1]; *h = old[:n-1]; return x }
+func (h resultHeap) Len() int           { return len(h) }
+func (h resultHeap) Less(i, j int) bool { return h[i].Seq < h[j].Seq }
+func (h resultHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *resultHeap) Push(x any)        { *h = append(*h, x.(Result)) }
+func (h *resultHeap) Pop() any          { old := *h; n := len(old); x := old[n-1]; *h = old[:n-1]; return x }
 
 func (s *Store) Scan(apex string, limit int) ([]Result, error) {
 	if limit <= 0 {
